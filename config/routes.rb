62021fa704e30439173e8e7b5f9d6eab5cfb6469
hodremlin <romanceofthecendrillon@gmail.com>
2017-11-27 22:30:56 +0000

    Added signup, puma

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
